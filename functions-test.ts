9c4b56e1dcf36e4cdb3dd17f648e26a84dd1f73c
Dave Wang <uberdwang@gmail.com>
2022-09-20 16:17:00 -0600

    Init commit

---
+++ functions-test.ts
@@ -0,0 +1,6 @@
+import addNumbers, { addStrings, getName } from './functions';
+
+console.log(addNumbers(1, 2));
+console.log(addStrings("a", "jack"));
+console.log(addStrings("a"));
+console.log(getName({ first: 'dave', last: 'wang'}));
---
+++ js-function-tests.js
@@ -0,0 +1,5 @@
+const { getName } = require('./functions');
+
+console.log(
+    getName()
+);
